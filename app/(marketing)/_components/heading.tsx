2fee3dfe787b7088da41e0be3e5a8a61cff906f1
aleksanderpalamar <apalamar@live.com>
2023-12-13 16:27:38 -0300

    ADD: added navbar in dashboard

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { BadgeComponent } from "@/components/badge-component";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -18,7 +19,6 @@ export const Heading = () => {
   const download = useDownload();
   const project = useProject();
   const [isMounted, setIsMounted] = useState(false);
-  
 
   useEffect(() => {
     setIsMounted(true);
@@ -36,28 +36,27 @@ export const Heading = () => {
   }
 
   return (
-    <div className="w-full space-y-4 flex flex-col">
+    <div className="w-full space-y-4 flex flex-col px-3 py-4">
       <h1 className="text-4xl self-start font-bold text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-orange-500">
         Aleksander Palamar
       </h1>
       <p className="text-gray-500 dark:text-gray-50 flex items-center gap-2">
-        Software Engineer |
-        <Badge className="text-violet-500 text-xs" variant="secondary">
-          Fullstack
+        Web Developer |
+        <Badge className="bg-violet-500/10 text-violet-500 hover:bg-violet-500/20 transition-all">
+          FullStack
         </Badge>
       </p>
       <p className="text-gray-500 dark:text-gray-50 word-spacing-2 break-words p-2 text-justify">
-        I am passionate about coding, and as a software engineer, I specialize
-        in incredible technologies such as JavaScript, React, TypeScript, and
-        Next.js. My professional journey is centered around crafting UI in
-        accordance with UX, ensuring that each web experience not only functions
-        seamlessly but also captivates users. While exploring these tools, I
-        discover new ways to turn ideas into reality, all while sharing my
-        enthusiasm through innovative projects and inspiring articles. I am here
-        not just to write code but to create engaging and impactful digital
+        I m passionate about coding and my favorite technologies are JavaScript,
+        React, TypeScript and Next.js. My professional journey is centered on
+        crafting UI according to UX, ensuring that each web experience not only
+        works perfectly, but also captivates users. By exploring these tools, I
+        discover new ways to turn ideas into reality, while sharing my
+        enthusiasm through innovative projects and blog posts. I m here to not
+        just write code, but to create engaging and impactful digital
         narratives.
       </p>
-      <div className="mt-4 flex items-center justify-center self-end space-x-2">        
+      <div className="mt-4 flex items-center justify-center self-end space-x-2">
         <Button
           className="hover:bg-emerald-100 dark:hover:bg-zinc-700/75"
           variant="ghost"
@@ -82,7 +81,7 @@ export const Heading = () => {
           size="sm"
         >
           More Projects
-        </Button>        
+        </Button>
       </div>
       <Separator className="my-4 w-full bg-muted-foreground/25 dark:bg-zinc-700" />
     </div>
--- components/badge-component.tsx
+++ components/badge-component.tsx
@@ -1,41 +1,55 @@
 "use client";
 
-import { Badge, BadgeProps } from "@/components/ui/badge";
-import { ActionTooltip } from "./action-tooltip";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+
+type LabelItemProps = {
+  label: string;
+  className?: string;
+};
 
 const labelItem = [
   {
     label: "React",
+    className: "bg-sky-500/10 text-sky-500 hover:bg-sky-500/20 transition-all",
   },
   {
     label: "Next.js",
+    className:
+      "bg-gray-950/10 text-gray-950 dark:text-gray-100 hover:bg-gray-950/20 transition-all",
   },
   {
     label: "Javascript",
+    className:
+      "bg-yellow-500/10 text-yellow-500 hover:bg-yellow-500/20 transition-all",
   },
   {
     label: "Tailwind",
+    className:
+      "bg-emerald-500/10 text-emerald-500 hover:bg-emerald-500/20 transition-all",
   },
   {
     label: "Typescript",
+    className:
+      "bg-blue-500/10 text-blue-500 hover:bg-blue-500/20 transition-all",
   },
   {
     label: "Node.js",
+    className:
+      "bg-lime-500/10  text-lime-500 hover:bg-lime-500/20 transition-all",
   },
   {
     label: "MySQL",
+    className: "bg-red-500/10 text-red-500 hover:bg-red-500/20 transition-all",
   },
-];
+] as LabelItemProps[];
 
 export const BadgeComponent = () => {
   const badgeLink = () => {
     return (
       <>
         {labelItem.map((item, index) => (
-          <Badge
-            key={index}
-            className="bg-violet-500 hover:bg-violet-400 transition-all"
-          >
+          <Badge key={index} className={cn("", item.className)}>
             {item.label}
           </Badge>
         ))}
