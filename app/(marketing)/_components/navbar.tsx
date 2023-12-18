345cd3a5a742f7d10785d6a16d194c22c1357462
aleksanderpalamar <apalamar@live.com>
2023-12-18 11:37:16 -0300

    feat: added signin button

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,31 +1,70 @@
 "use client";
 
 import { Logo } from "@/components/logo";
-import { MobileToggle } from "@/components/mobile-toggle"
+import { MobileToggle } from "@/components/mobile-toggle";
 import { ModeToggle } from "@/components/mode-toggle";
+import { cn } from "@/lib/utils";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { Loader } from "lucide-react";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export const Navbar = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
   useEffect(() => {
     setIsMounted(true);
-  }, [])
+  }, []);
 
   switch (isMounted) {
     case false:
-      return null
+      return null;
   }
 
   return (
-    <nav className="fixed z-50 top-0 w-full h-14 px-4 border-b
-    shadow-sm bg-violet-100 dark:bg-zinc-900 flex items-center">
+    <nav
+      className="fixed z-50 top-0 w-full h-14 px-4 border-b
+    shadow-sm bg-white/10 backdrop-blur-lg dark:bg-zinc-900/10 dark:backdrop-blur-lg flex items-center"
+    >
       <div className="flex items-center gap-x-4">
-       <Logo />         
+        <Logo />
       </div>
-      <div className="ml-auto flex items-center gap-8 pr-14">
-        <ModeToggle />        
+      <div className="ml-auto flex items-center gap-2 pr-4 animate-fade-left">
+        <Link
+          className={cn(
+            "p-1 border-l-2 border-r-2 border-zinc-700/75 hover:bg-zinc-500/10 text-base"
+          )}
+          role="link"
+          href="/blog"
+        >
+          Blog
+        </Link>
+        {isLoading && (
+          <Loader className="w-4 h-4 animate-spin text-gray-500 dark:text-gray-50" />
+        )}
+        {isAuthenticated && !isLoading && (
+          <div
+            className="p-1 border-l-2 border-r-2 border-zinc-700/75 hover:bg-zinc-500/10 text-base"
+            role="link"
+          >
+            <Link href="/documents" className="flex items-center gap-x-2">
+              Dashboard
+            </Link>
+          </div>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <SignInButton
+            mode="modal"
+            aria-label="Sign in"
+            className="p-1 border-l-2 border-r-2 border-zinc-700/75 hover:bg-zinc-500/10 text-base"
+          >
+            Área restrita
+          </SignInButton>
+        )}
+        <ModeToggle />
       </div>
     </nav>
-  )
-}
+  );
+};
