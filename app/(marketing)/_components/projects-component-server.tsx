38840f64e3daa264e7c243e45e7c15b6de97c307
aleksanderpalamar <apalamar@live.com>
2024-01-26 11:28:07 -0300

    Add 3D card components

--- app/(marketing)/_components/projects-component-server.tsx
+++ app/(marketing)/_components/projects-component-server.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import { getProjects } from "@/app/_service/projects";
 import { Translate } from "@/components/translate";
-import { Badge } from "@/components/ui/badge";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { cn } from "@/lib/utils";
 import { formatText } from "@/utils/formatText";
 import Link from "next/link";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-cards";
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 export const revalidate = 60; // 60 seconds
 export const dynamic = "force-dynamic";
@@ -24,71 +26,94 @@ export const ProjectsComponentServer = async () => {
     <div className="min-h-screen w-full flex flex-col items-center justify-start dark:bg-zinc-900">
       <div className="max-w-6xl p-2 space-y-4">
         <Translate />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 animate-fade-left">
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-4 animate-fade-left">
           {projects.map((project) => (
-            <div
-              key={project.id}
-              className="card bg-base-100 dark:bg-zinc-800 shadow-xl hover:shadow-2xl 
-              dark:shadow-2xl hover:scale-105 transition-all rounded overflow-hidden
-              flex flex-col space-y-4"
-            >
-              {project.coverImage ? (
-                <Dialog>
-                  <DialogTrigger>
-                    <img
-                      src={project.coverImage}
-                      alt={project.title}
-                      className="w-full h-48 object-cover"
-                    />
-                  </DialogTrigger>
-                  <DialogContent className="bg-white text-zinc-950 p-0 overflow-hidden max-w-6xl">
-                    <img
-                      src={project.coverImage}
-                      alt={project.title}
-                      className="w-full h-full object-cover animate-fade"
-                    />
-                  </DialogContent>
-                </Dialog>
-              ) : (
-                <div className="w-full h-48 bg-[#8257e6]">
-                  <div className="w-full h-full flex items-center justify-center">
-                    <p className="text-base font-bold text-zinc-50">
-                      No Cover Image
-                    </p>
-                  </div>
-                </div>
-              )}
-              <div className="p-4 space-y-4">
-                <h1 className="text-2xl font-bold">{project.title}</h1>
-                <p className="text-sm">{formatText(project.description, 10)}</p>
-                <div className="flex flex-wrap gap-2 items-center">
+            <CardContainer key={project.id} className="inter-var">
+              <CardBody
+                className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-violet-500/[0.1] 
+              dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border"
+              >
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  {project.title}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  {formatText(project.description, 10)}
+                </CardItem>
+                <CardItem translateZ="100" className="w-full mt-4">
+                  {project.coverImage ? (
+                    <Dialog>
+                      <DialogTrigger>
+                        <Image
+                          src={project.coverImage}
+                          alt={project.title}
+                          width={1000}
+                          height={1000}
+                          className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                        />
+                      </DialogTrigger>
+                      <DialogContent className="bg-white text-zinc-950 p-0 overflow-hidden max-w-6xl">
+                        <Image
+                          src={project.coverImage}
+                          alt={project.title}
+                          width={1000}
+                          height={1000}
+                          className="h-full w-full object-cover rounded-xl group-hover/card:shadow-xl animate-fade"
+                        />
+                      </DialogContent>
+                    </Dialog>
+                  ) : (
+                    <div className="w-full h-48 bg-[#8257e6]">
+                      <div className="w-full h-full flex items-center justify-center">
+                        <p className="text-base font-bold text-zinc-50">
+                          No Cover Image
+                        </p>
+                      </div>
+                    </div>
+                  )}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
                   {project.technologies.map((technology) => (
                     <Badge
                       key={technology}
                       className={cn(
-                        "bg-[#8257e6]/10 text-[#8257e6] hover:bg-[#8257e6]/20 transition-all w-[max-content]"
+                        "bg-[#8257e6]/10 text-[#8257e6] hover:bg-[#8257e6]/20 transition-all w-[max-content] mr-1"
                       )}
                     >
                       {technology}
                     </Badge>
                   ))}
-                </div>
-                <div className="flex items-center justify-between">
+                </CardItem>
+                <div className="flex justify-between items-center mt-20">
+                  <Link
+                    href={project.href}
+                    className="px-4 py-2 rounded-xl text-xs font-normal 
+                    dark:text-white"
+                    target="_blank"
+                  >
+                    Live →
+                  </Link>
                   <Link
                     href={project.githubUrl}
-                    className="p-1 border-l-2 border-r-2 border-zinc-700/75 hover:bg-zinc-500/10 text-base"
+                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black 
+                    text-white text-xs font-bold"
+                    target="_blank"
                   >
                     Github
                   </Link>
-                  <Link
-                    href={project.href}
-                    className="p-1 border-l-2 border-r-2 border-zinc-700/75 hover:bg-zinc-500/10 text-base"
-                  >
-                    Visit Site
-                  </Link>
                 </div>
-              </div>
-            </div>
+              </CardBody>
+            </CardContainer>
           ))}
         </div>
       </div>
